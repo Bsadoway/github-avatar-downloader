074002c187205b07c1c67f360fd7352f09ef959a
Blake Sadoway <blakesadoway@gmail.com>
2017-12-04 22:08:56 +0000

    Added headers options

--- download_avatars.js
+++ download_avatars.js
@@ -4,7 +4,16 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options ={
+    url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers = {
+      'User-Agent' : 'request',
+      'Authorization' : ''
+    }
+  };
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
