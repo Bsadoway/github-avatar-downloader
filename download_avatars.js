e60ebb057097a22a665bb0644b5bb80fe909f4a1
Blake Sadoway <blakesadoway@gmail.com>
2017-12-05 00:32:27 +0000

    turned JSON to object

--- download_avatars.js
+++ download_avatars.js
@@ -1,22 +1,26 @@
 var request = require('request');
-
+var secrets = require('./secrets')
 console.log('Welcome to the GitHub Avatar Downloader!');
-
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options ={
-    url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers = {
-      'User-Agent' : 'request',
-      'Authorization' : ''
-    }
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    json: true,
+    headers: {
+      'User-Agent' : 'request'
+
+    },
+    Authorization : secrets.GITHUB_TOKEN
   };
-  request(url, function(err, res, body) {
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
+
+  result.forEach(function(person){
+    console.log(person.avatar_url);
+  });
   console.log("Errors:", err);
-  console.log("Result:", result);
 });
