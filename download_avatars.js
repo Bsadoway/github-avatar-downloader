518eaa24f62ed8619b8e2bcebfa499c35ea9047c
Blake Sadoway <blakesadoway@gmail.com>
2017-12-05 00:50:32 +0000

    added downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var secrets = require('./secrets')
+var fs = require("fs");
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -16,6 +18,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
     cb(err, body);
   });
 }
+
+function downloadImageByURL(url, filePath) {
+
+  request
+    .get(url)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(reponse) {
+      console.log("Downloading image...");
+      console.log(reponse.statusMessage);
+      console.log(reponse.headers['content-type']);
+    })
+    .pipe(fs.createWriteStream(filePath))
+    .on('finish', function() {
+      console.log("Download complete.");
+    });
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./kvirani.jpg");
 
 getRepoContributors("jquery", "jquery", function(err, result) {
 
